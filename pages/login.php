302f2e78fde19bdb264e28fb8dc22bea433898f8
fifovalle <fifanaufal10@gmail.com>
2023-12-21 04:29:03 +0700

    Menambahkan file login

---
+++ pages/login.php
@@ -0,0 +1,82 @@
+<!-- - var bodyCustom = 'bg-blue bg-lighten-2' // Use any color palette class-->
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="CryptoDash admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
+    <meta name="keywords" content="admin template, CryptoDash Cryptocurrency Dashboard Template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+    <meta name="author" content="ThemeSelection">
+    <title>TUGAS BASDAT KELOMPOK 4</title>
+    <link rel="apple-touch-icon" href="app-assets/images/icons/1.jpg">
+    <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/icons/1.jpg">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i|Comfortaa:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/vendors.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/vendors/css/forms/icheck/icheck.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/vendors/css/forms/icheck/custom.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/app.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/core/menu/menu-types/vertical-compact-menu.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/vendors/css/cryptocoins/cryptocoins.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/pages/account-login.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+</head>
+
+<body class="vertical-layout vertical-compact-menu 1-column  bg-full-screen-image menu-expanded blank-page blank-page" data-open="click" data-menu="vertical-compact-menu" data-col="1-column">
+    <div class="app-content content">
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <section id="account-login" class="flexbox-container">
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <div class="col-xl-3 col-lg-4 col-md-5 col-sm-5 col-12 p-0 text-center d-none d-md-block">
+                            <div class="border-grey border-lighten-3 m-0 box-shadow-0 card-account-left height-400">
+                                <h1 style="color: white; padding-top: 175px; font-weight: bold">KELOMPOK 4</h1>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-5 col-sm-5 col-12 p-0">
+                            <div class="card border-grey border-lighten-3 m-0 box-shadow-0 card-account-right height-400">
+                                <div class="card-content">
+                                    <div class="card-body p-3">
+                                        <p class="text-center h5 text-capitalize">Selamat Datang</p>
+                                        <p class="mb-3 text-center">Di ATM Sederhana</p>
+                                        <form class="form-horizontal form-signin" action="index.html">
+                                            <fieldset class="form-label-group">
+                                                <input type="text" class="form-control" id="user-name" placeholder="No Rekening Anda" required="" autofocus="" name="no_rekening">
+                                                <label for="user-name">No Rekening Anda</label>
+                                            </fieldset>
+                                            <fieldset class="form-label-group">
+                                                <input type="password" class="form-control" id="user-password" placeholder="Kata Sandi Anda" required="" autofocus="" name="kata_sandi">
+                                                <label for="user-password">Kata Sandi Anda</label>
+                                            </fieldset>
+                                            <div class="form-group row">
+                                                <div class="col-md-6 col-12 text-center text-sm-left">
+                                                    <fieldset>
+                                                        <input type="checkbox" id="remember-me" class="chk-remember">
+                                                        <label for="remember-me"> Ingat Saya</label>
+                                                    </fieldset>
+                                                </div>
+                                                <div class="col-md-6 col-12 float-sm-left text-center text-sm-right"><a href="#" class="card-link">Lupa Sandi?</a></div>
+                                            </div>
+                                            <button type="submit" class="btn-gradient-primary btn-block my-1" name="masuk">Masuk</button>
+                                            <p class="text-center"><a href="account-register.html" class="card-link">Daftar</a></p>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+    <script src="../app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+    <script src="../app-assets/vendors/js/forms/icheck/icheck.min.js" type="text/javascript"></script>
+    <script src="../app-assets/js/core/app-menu.js" type="text/javascript"></script>
+    <script src="../app-assets/js/core/app.js" type="text/javascript"></script>
+    <script src="../app-assets/js/scripts/forms/form-login-register.js" type="text/javascript"></script>
+</body>
+
+</html>
