9760dcc40f5338e037d75d5531dbb6a22eb4de0a
fifovalle <fifanaufal10@gmail.com>
2023-12-20 14:34:55 +0700

    Menambahkan file atm dan file readme.md

---
+++ database/atm.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 20, 2023 at 08:19 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `modul9`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `atm`
+--
+
+CREATE TABLE `atm` (
+  `ID_ATM` int(11) NOT NULL,
+  `JENIS_ATM` enum('GOLD','SILVER','PREMIUM') NOT NULL,
+  `TANGGAL_PEMBUATAN_ATM` datetime NOT NULL,
+  `SALDO_AWAL` int(11) NOT NULL,
+  `JUMLAH_UANG_YANG_TERSEDIA` int(11) NOT NULL,
+  `LOKASI` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `layanan`
+--
+
+CREATE TABLE `layanan` (
+  `ID_LAYANAN` int(11) NOT NULL,
+  `ID_ATM` int(11) NOT NULL,
+  `JENIS_LAYANAN` varchar(225) NOT NULL,
+  `TANGGAL_LAYANAN` datetime NOT NULL,
+  `BIAYA_LAYANAN` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nasabah`
+--
+
+CREATE TABLE `nasabah` (
+  `ID_NASABAH` int(11) NOT NULL,
+  `NAMA` varchar(250) NOT NULL,
+  `NO_REKENING` int(16) NOT NULL,
+  `SALDO_REKENING` int(10) NOT NULL,
+  `ALAMAT_NASABAH` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `ID_TRANSAKSI` int(11) NOT NULL,
+  `ID_ATM` int(11) NOT NULL,
+  `ID_NASABAH` int(11) NOT NULL,
+  `ID_LAYANAN` int(11) NOT NULL,
+  `JENIS_TRANSAKSI` enum('TRANSFER','TARIK_TUNAI','MENABUNG') NOT NULL,
+  `JUMLAH_TRANSAKSI` int(10) NOT NULL,
+  `TANGGAL_DAN_WAKTU_TRANSAKSI` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `atm`
+--
+ALTER TABLE `atm`
+  ADD PRIMARY KEY (`ID_ATM`);
+
+--
+-- Indexes for table `layanan`
+--
+ALTER TABLE `layanan`
+  ADD PRIMARY KEY (`ID_LAYANAN`),
+  ADD UNIQUE KEY `ID_ATM` (`ID_ATM`);
+
+--
+-- Indexes for table `nasabah`
+--
+ALTER TABLE `nasabah`
+  ADD PRIMARY KEY (`ID_NASABAH`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`ID_TRANSAKSI`),
+  ADD UNIQUE KEY `ID_ATM` (`ID_ATM`),
+  ADD UNIQUE KEY `ID_NASABAH` (`ID_NASABAH`),
+  ADD UNIQUE KEY `ID_LAYANAN` (`ID_LAYANAN`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`ID_ATM`) REFERENCES `atm` (`ID_ATM`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`ID_NASABAH`) REFERENCES `nasabah` (`ID_NASABAH`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`ID_LAYANAN`) REFERENCES `layanan` (`ID_LAYANAN`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
