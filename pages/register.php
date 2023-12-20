ba507e8d938efe1466a17ceaec8a9b3bbc182bb4
fifovalle <fifanaufal10@gmail.com>
2023-12-21 04:35:12 +0700

    Menambahkan file register dan modifikasi file login

--- pages/register.php
+++ pages/register.php
@@ -40,8 +40,8 @@
                             <div class="card border-grey border-lighten-3 m-0 box-shadow-0 card-account-right height-400">
                                 <div class="card-content">
                                     <div class="card-body p-3">
-                                        <p class="text-center h5 text-capitalize">Selamat Datang</p>
-                                        <p class="mb-3 text-center">Di ATM Sederhana</p>
+                                        <p class="text-center h5 text-capitalize">Mulai Dengan ATM</p>
+                                        <p class="mb-3 text-center">Silahkan Buat Akun Anda</p>
                                         <form class="form-horizontal form-signin" action="index.html">
                                             <fieldset class="form-label-group">
                                                 <input type="text" class="form-control" id="user-name" placeholder="No Rekening Anda" required="" autofocus="" name="no_rekening">
@@ -52,16 +52,15 @@
                                                 <label for="user-password">Kata Sandi Anda</label>
                                             </fieldset>
                                             <div class="form-group row">
-                                                <div class="col-md-6 col-12 text-center text-sm-left">
+                                                <div class="col-12 text-center text-sm-left">
                                                     <fieldset>
                                                         <input type="checkbox" id="remember-me" class="chk-remember">
-                                                        <label for="remember-me"> Ingat Saya</label>
+                                                        <label for="remember-me"> Saya setuju dengan syarat ATM</label>
                                                     </fieldset>
                                                 </div>
-                                                <div class="col-md-6 col-12 float-sm-left text-center text-sm-right"><a href="#" class="card-link">Lupa Sandi?</a></div>
                                             </div>
-                                            <button type="submit" class="btn-gradient-primary btn-block my-1" name="masuk">Masuk</button>
-                                            <p class="text-center"><a href="register.php" class="card-link">Daftar</a></p>
+                                            <button type="submit" class="btn-gradient-primary btn-block my-1" name="masuk">Daftar</button>
+                                            <p class="text-center"><a href="login.php" class="card-link">Masuk</a></p>
                                         </form>
                                     </div>
                                 </div>
