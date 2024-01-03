1396316d2f3f89f821a9b31d3a36fdb7c0dc4536
fifovalle <fifanaufal10@gmail.com>
2024-01-03 13:52:19 +0700

    Memodifikasi file footer

--- partials/footer.php
+++ partials/footer.php
@@ -2,7 +2,7 @@
     <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">Hak Cipta &copy;
             <script>
                 document.write(new Date().getFullYear())
-            </script> <a class="text-bold-800 grey darken-2" href="#" target="_blank">Kelompok 4 </a>, Seluruh Hak
+            </script> <a class="text-bold-800 grey darken-2" href="#" target="_blank">Naufal </a>, Seluruh Hak
             Cipta
-        </span><span class="float-md-right d-block d-md-inline-blockd-none d-lg-block">Kelompok 4 <i class="ft-heart pink"></i></span></p>
+        </span><span class="float-md-right d-block d-md-inline-blockd-none d-lg-block">FIF OVALLE <i class="ft-heart pink"></i></span></p>
 </footer>
